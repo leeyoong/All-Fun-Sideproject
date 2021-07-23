c632d646cfc0676be880152e8367b67325441452
leeyoong <nngeet010@naver.com>
2021-07-23 15:25:12 +0900

    upload ServerSide
    자바 : 16

---
+++ Server/SideProject/src/main/java/AllFun/SideProject/SideProjectApplication.java
@@ -0,0 +1,13 @@
+package AllFun.SideProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SideProjectApplication.class, args);
+	}
+
+}
---
+++ Server/SideProject/src/test/java/AllFun/SideProject/SideProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package AllFun.SideProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
