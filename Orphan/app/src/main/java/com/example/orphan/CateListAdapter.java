e136cd78edf977001f522ff105f93b374e3ceef0
alstn0723 <66290861+alstn0723@users.noreply.github.com>
2021-08-09 01:41:09 +0900

    ㄴㅇㄹ
    ㅁㄴㅇㄹ

---
+++ Orphan/app/src/main/java/com/example/orphan/CateListAdapter.java
@@ -0,0 +1,89 @@
+package com.example.orphan;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class CateListAdapter extends BaseAdapter {
+
+    private TextView catetext;
+    private TextView catenow;
+    private TextView catemax;
+
+
+
+    private ArrayList<CateListItem> cateListItemsList = new ArrayList<CateListItem>();
+
+    public  CateListAdapter(){
+
+    }
+
+    @Override
+    public int getCount() {
+        return cateListItemsList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return cateListItemsList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final int pos = position;
+        final Context context = parent.getContext();
+
+        if (convertView == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.catelist, parent, false);
+        }
+        TextView catetitleTextView = (TextView) convertView.findViewById(R.id.catetitle);
+        TextView catenowTextView = (TextView) convertView.findViewById(R.id.catenow);
+        TextView catemaxTextView = (TextView) convertView.findViewById(R.id.catemax);
+
+
+
+
+        CateListItem cateListItem = cateListItemsList.get(position);
+
+        catetitleTextView.setText(cateListItem.getCatetitleStr());
+        catenowTextView.setText(cateListItem.getCatenowStr());
+        catemaxTextView.setText(cateListItem.getCatemaxStr());
+
+
+
+        LinearLayout catetouch =  (LinearLayout) convertView.findViewById(R.id.catetouch);
+
+
+
+        return convertView;
+
+    }
+
+    public void addItem(String catetitleStr, String catenowStr, String catemaxStr){
+        CateListItem item = new CateListItem();
+
+        item.setCatetitleStr(catetitleStr);
+        item.setCatenowStr(catenowStr);
+        item.setCatemaxStr(catemaxStr);
+
+
+        cateListItemsList.add(item);
+
+    }
+}
--- Orphan/app/src/main/java/com/example/orphan/MatchMake.java
+++ Orphan/app/src/main/java/com/example/orphan/MatchMake.java
@@ -6,13 +6,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MatchMake extends AppCompatActivity {
+
+
+    private CateListAdapter adapter;
+    private ListView listView;
+
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            // pre-condition
+            return;
+        }
+
+        int totalHeight = 0;
+
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.AT_MOST);
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            //listItem.measure(0, 0);
+            listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+
+        params.height = totalHeight;
+        listView.setLayoutParams(params);
+
+        listView.requestLayout();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +52,27 @@ public class MatchMake extends AppCompatActivity {
         setContentView(R.layout.activity_match_make);
 
         Button cateadd = (Button) findViewById(R.id.cateadd);
+
+
+
+        adapter = new CateListAdapter();
+
+
+        listView = (ListView) findViewById(R.id.cateview);
+        listView.setAdapter(adapter);
+
+
+        adapter.addItem("프론트","1","2");
+        adapter.addItem("백","1","1");
+        adapter.addItem("AI","0","1");
+
+
+
+        setListViewHeightBasedOnChildren(listView);
+        adapter.notifyDataSetChanged();
+
+
+
 
         cateadd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +98,8 @@ public class MatchMake extends AppCompatActivity {
                     aBuilder.setView(mView);
                     AlertDialog dialog = aBuilder.create();
                     dialog.show();
+
+
             }
         });
     }
