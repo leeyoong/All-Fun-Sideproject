f7e138e6d44db0d8c9873647a193a7d5735cc6f8
alstn0723 <66290861+alstn0723@users.noreply.github.com>
2021-08-09 02:11:16 +0900

    매치 리스트뷰
    매치가서 작성글 눌러서 밑으로 내려보셈

---
+++ Orphan/app/src/main/java/com/example/orphan/PostListItem.java
@@ -0,0 +1,40 @@
+package com.example.orphan;
+
+public class PostListItem {
+
+
+    private String posttitleStr;
+    private String postnowStr;
+    private String postmaxStr;
+
+    public String getPosttitleStr() {
+        return posttitleStr;
+    }
+
+    public void setPosttitleStr(String posttitleStr) {
+        this.posttitleStr = posttitleStr;
+    }
+
+    public String getPostnowStr() {
+        return postnowStr;
+    }
+
+    public void setPostnowStr(String postnowStr) {
+        this.postnowStr = postnowStr;
+    }
+
+    public String getPostmaxStr() {
+        return postmaxStr;
+    }
+
+    public void setPostmaxStr(String postmaxStr) {
+        this.postmaxStr = postmaxStr;
+    }
+
+
+
+
+
+
+
+}
