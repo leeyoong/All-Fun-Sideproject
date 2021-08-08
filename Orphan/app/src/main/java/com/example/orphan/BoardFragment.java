5e063eddf4fb971becbad632f2e857e14671521f
alstn0723 <66290861+alstn0723@users.noreply.github.com>
2021-08-08 19:40:31 +0900

    hi
    hi

--- Orphan/app/src/main/java/com/example/orphan/BoardFragment.java
+++ Orphan/app/src/main/java/com/example/orphan/BoardFragment.java
@@ -1,5 +1,6 @@
 package com.example.orphan;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 /**
@@ -68,6 +71,8 @@ public class BoardFragment extends Fragment {
 
         adapter = new BoardListAdapter();
 
+        ImageView boardpencil = (ImageView) view.findViewById(R.id.boardpencil);
+
         listView = (ListView) view.findViewById(R.id.boardview);
         listView.setAdapter(adapter);
 
@@ -77,6 +82,14 @@ public class BoardFragment extends Fragment {
 
 
         adapter.notifyDataSetChanged();
+
+        boardpencil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Intent = new Intent(view.getContext(), BoardMake.class);
+                view.getContext().startActivity(Intent);
+            }
+        });
 
 
         return view;
---
+++ Orphan/app/src/main/java/com/example/orphan/BoardMake.java
@@ -0,0 +1,14 @@
+package com.example.orphan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BoardMake extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board_make);
+    }
+}
