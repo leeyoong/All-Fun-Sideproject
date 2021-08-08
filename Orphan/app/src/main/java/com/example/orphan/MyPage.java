a12a3889af20d8e7ae89fc959d2e3f2519da97b6
alstn0723 <66290861+alstn0723@users.noreply.github.com>
2021-08-08 21:46:04 +0900

    asef
    afse

---
+++ Orphan/app/src/main/java/com/example/orphan/MyPage.java
@@ -0,0 +1,14 @@
+package com.example.orphan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_page);
+    }
+}
